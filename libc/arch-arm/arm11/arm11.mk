54643c8e0432557f50670f21a636d6b15fa60856
AndroidMeda <hephappy@gmail.com>
2013-08-13 17:48:48 +0300

    bionic(cm-12): Implement armv6 optimized strlen from glibc 2.18
    Change-Id: Ief985c9ca08cb74d9d7e41d3a583e62699f745cf

---
+++ libc/arch-arm/arm11/arm11.mk
@@ -0,0 +1,13 @@
+libc_bionic_src_files_arm += \
+    arch-arm/arm11/bionic/strlen.S
+
+libc_bionic_src_files_arm += \
+    arch-arm/generic/bionic/memcpy.S \
+    arch-arm/generic/bionic/memset.S \
+    arch-arm/generic/bionic/strcmp.S \
+    arch-arm/generic/bionic/strcpy.S \
+    bionic/memchr.c \
+    bionic/memmove.c \
+    bionic/__strcat_chk.cpp \
+    bionic/__strcpy_chk.cpp \
+    upstream-openbsd/lib/libc/string/strcat.c \
