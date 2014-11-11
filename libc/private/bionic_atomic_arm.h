3658a1b1a25812558cd3f8be6b2c06ace10cb18b
Marco Milanese <marcomilanese7@gmail.com>
2014-11-11 13:45:01 +0200

    bionic: Fix atomic support for thumb1
    ldrex and strex are arm opcodes
    
    Change-Id: I41e0059d405998ea738ebcbf8cbb5716fee572d1

--- libc/private/bionic_atomic_arm.h
+++ libc/private/bionic_atomic_arm.h
@@ -16,6 +16,27 @@
 #ifndef BIONIC_ATOMIC_ARM_H
 #define BIONIC_ATOMIC_ARM_H
 
+#if defined(__thumb__) && !defined(__thumb2__)
+#  define  __ATOMIC_SWITCH_TO_ARM \
+            "adr r3, 5f\n" \
+            "bx  r3\n" \
+            ".align\n" \
+            ".arm\n" \
+        "5:\n"
+/* note: the leading \n below is intentional */
+#  define __ATOMIC_SWITCH_TO_THUMB \
+            "\n" \
+            "adr r3, 6f+1\n" \
+            "bx  r3\n" \
+            ".thumb\n" \
+        "6:\n"
+#  define __ATOMIC_CLOBBERS   "r3",  /* list of clobbered registers */
+#else
+#  define  __ATOMIC_SWITCH_TO_ARM   /* nothing */
+#  define  __ATOMIC_SWITCH_TO_THUMB /* nothing */
+#  define  __ATOMIC_CLOBBERS        /* nothing */
+#endif /* defined(__thumb__) && !defined(__thumb2__) */
+
 __ATOMIC_INLINE__ void __bionic_memory_barrier() {
 #if defined(ANDROID_SMP) && ANDROID_SMP == 1
   __asm__ __volatile__ ( "dmb ish" : : : "memory" );
@@ -33,6 +54,7 @@ __ATOMIC_INLINE__ int __bionic_cmpxchg(int32_t old_value, int32_t new_value, vol
   int32_t prev, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%3]\n"
           "mov %1, #0\n"
           "teq %0, %4\n"
@@ -40,9 +62,10 @@ __ATOMIC_INLINE__ int __bionic_cmpxchg(int32_t old_value, int32_t new_value, vol
           "it eq\n"
 #endif
           "strexeq %1, %5, [%3]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (status), "+m"(*ptr)
           : "r" (ptr), "Ir" (old_value), "r" (new_value)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev != old_value;
 }
@@ -52,11 +75,13 @@ __ATOMIC_INLINE__ int32_t __bionic_swap(int32_t new_value, volatile int32_t* ptr
   int32_t prev, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%3]\n"
           "strex %1, %4, [%3]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (status), "+m" (*ptr)
           : "r" (ptr), "r" (new_value)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev;
 }
@@ -66,12 +91,14 @@ __ATOMIC_INLINE__ int32_t __bionic_atomic_dec(volatile int32_t* ptr) {
   int32_t prev, tmp, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%4]\n"
           "sub %1, %0, #1\n"
           "strex %2, %1, [%4]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (tmp), "=&r" (status), "+m"(*ptr)
           : "r" (ptr)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev;
 }
